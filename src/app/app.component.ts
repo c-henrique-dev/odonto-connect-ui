73bc01e50142c681af3a6b1b6779d9c5b22b7b4d
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-07-02 16:57:29 -0300

    refactor: excluir codificação desnecessária

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { MenubarComponent } from './components/menubar/menubar.component';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [MenubarComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'odonto-connect-ui';
-
-  constructor(private router: Router, private route: ActivatedRoute) {}
-
-  isLoginPageActive(): boolean {
-    return this.router.isActive('/auth/login', true) && this.route.outlet === 'login';
-  }
-}
+export class AppComponent {}
